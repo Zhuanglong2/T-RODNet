5255b6642f24e28dd4a12ce50c951e60b68fd994
Zhuanglong <58128328+Zhuanglong2@users.noreply.github.com>
2025-05-15 16:00:09 +0800

    Create loss_history.py

---
+++ tools/loss_history.py
@@ -0,0 +1,92 @@
+import os
+
+import scipy.signal
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from matplotlib import pyplot as plt
+from torch import nn
+
+
+def CE_Loss(inputs, target, num_classes=21):
+    n, c, h, w = inputs.size()
+    nt, ht, wt = target.size()
+    if h != ht and w != wt:
+        inputs = F.interpolate(inputs, size=(ht, wt), mode="bilinear", align_corners=True)
+
+    temp_inputs = inputs.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    temp_target = target.view(-1)
+
+    CE_loss = nn.NLLLoss(ignore_index=num_classes)(F.log_softmax(temp_inputs, dim=-1), temp_target)
+    return CE_loss
+
+
+def Dice_loss(inputs, target, beta=1, smooth=1e-5):
+    n, c, h, w = inputs.size()
+    nt, ht, wt, ct = target.size()
+
+    if h != ht and w != wt:
+        inputs = F.interpolate(inputs, size=(ht, wt), mode="bilinear", align_corners=True)
+    temp_inputs = torch.softmax(inputs.transpose(1, 2).transpose(2, 3).contiguous().view(n, -1, c), -1)
+    temp_target = target.view(n, -1, ct)
+
+    # --------------------------------------------#
+    #   计算dice loss
+    # --------------------------------------------#
+    tp = torch.sum(temp_target[..., :-1] * temp_inputs, axis=[0, 1])
+    fp = torch.sum(temp_inputs, axis=[0, 1]) - tp
+    fn = torch.sum(temp_target[..., :-1], axis=[0, 1]) - tp
+
+    score = ((1 + beta ** 2) * tp + smooth) / ((1 + beta ** 2) * tp + beta ** 2 * fn + fp + smooth)
+    dice_loss = 1 - torch.mean(score)
+    return dice_loss
+
+
+class LossHistory():
+    def __init__(self, log_dir, model_name):
+        import datetime
+        curr_time = datetime.datetime.now()
+        time_str = datetime.datetime.strftime(curr_time, '%Y_%m_%d_%H_%M_%S')
+        self.log_dir = log_dir
+        self.time_str = time_str
+        self.save_path = os.path.join(self.log_dir, model_name + "_" + "loss_" + str(self.time_str))
+        self.losses = []
+        self.val_loss = []
+        self.precision = []
+        self.model_name = []
+
+        os.makedirs(self.save_path)
+
+    def append_loss(self, loss):
+        self.losses.append(loss)
+        with open(os.path.join(self.save_path, "epoch_loss_" + str(self.time_str) + ".txt"), 'a') as f:
+            f.write(str(loss))
+            f.write("\n")
+
+        self.loss_plot()
+
+    def loss_plot(self):
+        iters = range(len(self.losses))
+
+        plt.figure()
+        plt.plot(iters, self.losses, 'red', linewidth=2, label='train loss')
+
+        try:
+            if len(self.losses) < 25:
+                num = 5
+            else:
+                num = 15
+
+            plt.plot(iters, scipy.signal.savgol_filter(self.losses, num, 3), 'green', linestyle='--', linewidth=2,
+                     label='smooth train loss')
+        except:
+            pass
+
+        plt.grid(True)
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend(loc="upper right")
+
+        plt.savefig(os.path.join(self.save_path, "epoch_loss_" + str(self.time_str) + ".png"))
+        plt.close()#关闭 节省内存
+
